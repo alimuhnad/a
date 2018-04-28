4b360f5be7e341dc691012df975c364a3b25a94b
alimuhnad <alimuhnad72@gmail.com>
2018-04-28 12:52:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -65,10 +65,8 @@ var items = mongoose.model('items', {
 
 var user = new users();
 
+app.use( express.static(__dirname + '/uploads'));
 
-app.get('/uploads', function (req, res) {
-    res.get(path.resolve('./uploads/'))
-}); 
 
 app.post('/api/login', function(req, res) {
     var username = req.body.username;
