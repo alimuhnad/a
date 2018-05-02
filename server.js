27c00a4aeed79fbeb320e93e7345764be81f7d3f
alimuhnad <alimuhnad72@gmail.com>
2018-05-02 11:21:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var morgan = require('morgan');             // log requests to the console (expr
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 var cors = require('cors');
 //رفع صورة واحدة
@@ -31,8 +29,9 @@ mongoose.connect('mongodb://a:a@ds161539.mlab.com:61539/a');
 //mongoose.connect('mongodb://a:a@ds161539.mlab.com:61539/a');
 
 app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 app.use(cors());
